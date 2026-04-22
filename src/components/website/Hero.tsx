7b349ec0ccf449489a18549bf66f96ef459c6cae
samveltorosyan <samotorosyan7@gmail.com>
2026-04-22 17:04:42 +0400

    style: move hero watermark to bottom left and increase visibility

--- src/components/website/Hero.tsx
+++ src/components/website/Hero.tsx
@@ -18,13 +18,13 @@ export default function Hero() {
             <div className="absolute -top-32 -right-32 w-[600px] h-[600px] bg-primary/5 rounded-full blur-[100px] pointer-events-none"></div>
             <div className="absolute bottom-0 -left-32 w-[500px] h-[500px] bg-blue-400/10 rounded-full blur-[80px] pointer-events-none"></div>
             
-            {/* Subtle Left-aligned Logo Watermark */}
-            <div className="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/4 opacity-[0.02] pointer-events-none z-0 select-none hidden lg:block">
+            {/* Subtle Bottom-left Logo Watermark */}
+            <div className="absolute bottom-0 left-0 -translate-x-1/4 translate-y-1/4 opacity-[0.05] pointer-events-none z-0 select-none hidden lg:block">
                 <Image 
                     src="/logo.png" 
                     alt="Retrieve Logo" 
-                    width={1000} 
-                    height={1000} 
+                    width={800} 
+                    height={800} 
                     className="grayscale object-contain"
                     priority
                 />
